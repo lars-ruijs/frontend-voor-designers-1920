45992e42367c3dfd35c0f5081c2cd5830ee06f1d
lars <60745347+lars-ruijs@users.noreply.github.com>
2020-02-26 21:00:42 +0100

    Intergratie SortableJS
    Drag and drop playlist nu via SortableJS library.
    
    Standaard styling aangepast.
    
    Comments toegevoegd.

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -1,108 +1,62 @@
-/*
-  Wat doe je ook alweer in Javascript voor een micro-interactie?
-  1. Gebruik de querySelector om een element in je html te selecteren
-  2. Koppel een evenListener aan het element om een mouse-event te detecteren
-  3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
-*/
 
-//Element dat gesleept wordt
-function dragStart(element) {
-  this.style.opacity = '0.4'; //Voeg doorzichtigheid toe
-  dragSrcEl = this; //Variabele dragSrcEl bevat het html LI-item
-  element.dataTransfer.effectAllowed = 'move'; //Slepen
-  element.dataTransfer.setData('text/html', this.innerHTML);
-}
 
-//Voeg een class 'over' toe wanneer je  met iets sleepbaars over een element gaat
-function dragEnter(element) {
-  this.classList.add('over');
-}
+///////////////////////////////////////////////////
+/////////////// SORTABLE JS LIBRARY ///////////////
+/// via: https://github.com/SortableJS/Sortable ///
+///////////////////////////////////////////////////
 
-//Slepen eindigt
-function dragEnd(element) {
-  var listItems = document.querySelectorAll('.sleepbaar');
-  [].forEach.call(listItems, function(item) {
-    item.classList.remove('over');
+  var deLijst = document.querySelector('#dragable-list'); //De lijst (playlist)
+
+  var sortable = Sortable.create(deLijst, { //Laat Sortable een sleepbare lijst maken van 'deLijst'
+    animation: 250,  // Animatie snelheid in ms
+  	easing: "cubic-bezier(1, 0, 0, 1)",
+    disabled: true, //De 'drag and drop' functie is allereest uitgeschakeld
   });
-  this.style.opacity = '1';
-}
 
-function dragOver(element) {
-  element.preventDefault();
-  element.dataTransfer.dropEffect = 'move';
-  return false;
-}
+///////////////////////////////////////////////////
 
-function dragLeave(element) {
-  element.stopPropagation();
-  this.classList.remove('over');
-}
+document.querySelector('.wijzig').addEventListener("click", function() {//Kijk naar clicks bij button
+  // console.log(this);
+  var icoon = document.querySelectorAll('.reorder-icon'); //Selecteer alle 'sorteer' icons
 
-function dragDrop(e) {
-  if (dragSrcEl != this) {
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-  return false;
-}
+  // Sortable: check of de sleepbare lijst is disabled (true of false).
+  // Bron via: https://github.com/SortableJS/Sortable#disabled-options
+      var state = sortable.option("disabled");
 
-var listItems = document.querySelectorAll('.sleepbaar');
-[].forEach.call(listItems, function(item) {
-  addEventsDragAndDrop(item);
+  var lijstItem = document.querySelectorAll('.list-element'); //Selecteer de list items
+
+  if (this.firstChild.data == "Wijzig") //Wanneer de tekst in de knop 'Wijzig' is, doe onderstaande
+   {
+      // console.log('Wijzig wordt gereed');
+      this.firstChild.data = "Gereed"; //Tekst in de knop wordt nu 'Gereed'
+
+      // Sortable: verander de waarde van 'disabled' naar iets anders dan huidige state.
+      // Bron via: https://github.com/SortableJS/Sortable#disabled-options
+          sortable.option("disabled", !state);
+
+      lijstItem.forEach(function(item){ //Voor elk list item wordt de cursor gewijzigd naar 'move'
+        item.style.cursor = "move";
+      });
+
+      icoon.forEach(function(item){ //Voor alle 'sorteer' icons, verwijder class '.hidden'
+        item.classList.remove("hidden");
+      });
+   }
+   else //In alle andere gevallen, doe onderstaande
+   {
+     // console.log('Gereed wordt Wijzig');
+     this.firstChild.data = "Wijzig"; //Tekst in de knop wordt nu 'Wijzig'
+
+     // Sortable: verander de waarde van 'disabled' naar iets anders dan huidige state.
+     // Bron via: https://github.com/SortableJS/Sortable#disabled-options
+          sortable.option("disabled", !state);
+
+     lijstItem.forEach(function(item){ //Voor elk list item wordt de cursor gewijzigd naar 'default'
+       item.style.cursor = "default";
+     });
+
+     icoon.forEach(function(item){ //Voor alle 'sorteer' icons, voeg class '.hidden' toe
+       item.classList.add("hidden");
+     });
+   }
 });
-
-function addEventsDragAndDrop(el) {
-  el.addEventListener('dragstart', dragStart, false);
-  el.addEventListener('dragenter', dragEnter, false);
-  el.addEventListener('dragover', dragOver, false);
-  el.addEventListener('dragleave', dragLeave, false);
-  el.addEventListener('drop', dragDrop, false);
-  el.addEventListener('dragend', dragEnd, false);
-}
-
-
-
-// document.addEventListener("dragstart", function(element) {
-//   // store a ref. on the dragged elem
-//   dragged = element.target;
-//   // make it half transparent
-//   element.target.style.opacity = '0.4';
-//   event.dataTransfer.effectAllowed = "move";
-// }, false);
-//
-// document.addEventListener("dragend", function(event) {
-//   // reset the transparency
-//   event.target.style.opacity = "";
-// }, false);
-//
-// document.addEventListener("dragover", function(event) {
-//   // prevent default to allow drop
-//   event.preventDefault();
-// }, false);
-//
-// document.addEventListener("dragenter", function(event) {
-//   // highlight potential drop target when the draggable element enters it
-//   if (event.target.className == "dropzone") {
-//     event.target.style.background = "purple";
-//   }
-//
-// }, false);
-//
-// document.addEventListener("dragleave", function(event) {
-//   // reset background of potential drop target when the draggable element leaves it
-//   if (event.target.className == "dropzone") {
-//     event.target.style.background = "";
-//   }
-//
-// }, false);
-//
-// document.addEventListener("drop", function(event) {
-//   // prevent default action (open as link for some elements)
-//   event.preventDefault();
-//   // move dragged elem to the selected drop target
-//   if (event.target.className == "dropzone") {
-//     event.target.style.background = "";
-//     dragged.parentNode.removeChild( dragged );
-//     event.target.appendChild( dragged );
-//   }
-// }, false);
